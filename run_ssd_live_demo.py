722ba3ec40feaf2457cb321b17c6d63cb0c02b51
shaoshengsong <30892500+shaoshengsong@users.noreply.github.com>
2019-06-24 12:11:32 +0800

    Add files via upload

---
+++ run_ssd_live_demo.py
@@ -0,0 +1,83 @@
+from vision.ssd.vgg_ssd import create_vgg_ssd, create_vgg_ssd_predictor
+from vision.ssd.mobilenetv1_ssd import create_mobilenetv1_ssd, create_mobilenetv1_ssd_predictor
+from vision.ssd.mobilenetv1_ssd_lite import create_mobilenetv1_ssd_lite, create_mobilenetv1_ssd_lite_predictor
+from vision.ssd.squeezenet_ssd_lite import create_squeezenet_ssd_lite, create_squeezenet_ssd_lite_predictor
+from vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite, create_mobilenetv2_ssd_lite_predictor
+from vision.utils.misc import Timer
+import cv2
+import sys
+
+if len(sys.argv) < 4:
+    print('Usage: python run_ssd_example.py <net type>  <model path> <label path> [video file]')
+    sys.exit(0)
+net_type = sys.argv[1]
+model_path = sys.argv[2]
+label_path = sys.argv[3]
+
+if len(sys.argv) >= 5:
+    cap = cv2.VideoCapture(sys.argv[4])  # capture from file
+else:
+    cap = cv2.VideoCapture(0)   # capture from camera
+    cap.set(3, 1920)
+    cap.set(4, 1080)
+
+class_names = [name.strip() for name in open(label_path).readlines()]
+num_classes = len(class_names)
+
+
+if net_type == 'vgg16-ssd':
+    net = create_vgg_ssd(len(class_names), is_test=True)
+elif net_type == 'mb1-ssd':
+    net = create_mobilenetv1_ssd(len(class_names), is_test=True)
+elif net_type == 'mb1-ssd-lite':
+    net = create_mobilenetv1_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'mb2-ssd-lite':
+    net = create_mobilenetv2_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'sq-ssd-lite':
+    net = create_squeezenet_ssd_lite(len(class_names), is_test=True)
+else:
+    print("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
+    sys.exit(1)
+net.load(model_path)
+
+if net_type == 'vgg16-ssd':
+    predictor = create_vgg_ssd_predictor(net, candidate_size=200)
+elif net_type == 'mb1-ssd':
+    predictor = create_mobilenetv1_ssd_predictor(net, candidate_size=200)
+elif net_type == 'mb1-ssd-lite':
+    predictor = create_mobilenetv1_ssd_lite_predictor(net, candidate_size=200)
+elif net_type == 'mb2-ssd-lite':
+    predictor = create_mobilenetv2_ssd_lite_predictor(net, candidate_size=200)
+elif net_type == 'sq-ssd-lite':
+    predictor = create_squeezenet_ssd_lite_predictor(net, candidate_size=200)
+else:
+    print("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
+    sys.exit(1)
+
+
+timer = Timer()
+while True:
+    ret, orig_image = cap.read()
+    if orig_image is None:
+        continue
+    image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+    timer.start()
+    boxes, labels, probs = predictor.predict(image, 10, 0.4)
+    interval = timer.end()
+    print('Time: {:.2f}s, Detect Objects: {:d}.'.format(interval, labels.size(0)))
+    for i in range(boxes.size(0)):
+        box = boxes[i, :]
+        label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
+        cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
+
+        cv2.putText(orig_image, label,
+                    (box[0]+20, box[1]+40),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,  # font scale
+                    (255, 0, 255),
+                    2)  # line type
+    cv2.imshow('annotated', orig_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
---
+++ vision/ssd/ssd.py
@@ -0,0 +1,170 @@
+import torch.nn as nn
+import torch
+import numpy as np
+from typing import List, Tuple
+import torch.nn.functional as F
+
+from ..utils import box_utils
+from collections import namedtuple
+GraphPath = namedtuple("GraphPath", ['s0', 'name'])  #
+
+
+class SSD(nn.Module):
+    def __init__(self, num_classes: int, base_net: nn.ModuleList, source_layer_indexes: List[int],
+                 extras: nn.ModuleList, classification_headers: nn.ModuleList,
+                 regression_headers: nn.ModuleList, is_test=False, config=None, device=None):
+        """Compose a SSD model using the given components.
+        """
+        super(SSD, self).__init__()
+
+        self.num_classes = num_classes
+        self.base_net = base_net
+        self.source_layer_indexes = source_layer_indexes
+        self.extras = extras
+        self.classification_headers = classification_headers
+        self.regression_headers = regression_headers
+        self.is_test = is_test
+        self.config = config
+
+        self.source_layer_add_ons = nn.ModuleList([t[1] for t in source_layer_indexes
+                                                   if isinstance(t, tuple) and not isinstance(t, GraphPath)])
+        if device:
+            self.device = device
+        else:
+            self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        if is_test:
+            self.config = config
+            self.priors = config.priors.to(self.device)
+            
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        confidences = []
+        locations = []
+        start_layer_index = 0
+        header_index = 0
+        for index_0, end_layer_index in enumerate(self.source_layer_indexes):#总共要循环两次
+
+            if isinstance(end_layer_index, GraphPath):#第一次循环执行
+                path = end_layer_index
+                end_layer_index = end_layer_index.s0
+                added_layer = None
+            else:#第二次循环执行
+                added_layer = None
+                path = None
+
+
+            for index_1,layer in enumerate(self.base_net[start_layer_index: end_layer_index]):
+                x = layer(x)
+            if added_layer:
+                y = added_layer(x)
+            else:
+                y = x
+
+  
+            if path:#只在第一次循环有值 GraphPath(s0=11, name='conv', s1=-1)
+                sub = getattr(self.base_net[end_layer_index], path.name)
+                for index_2,layer in enumerate(sub):
+                    y = layer(y)
+                # sub: Sequential(
+                #   (0): Conv2d(48, 288, kernel_size=(1, 1), stride=(1, 1), bias=False)
+                #   (1): BatchNorm2d(288, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+                #   (2): h_swish()
+                # )
+
+
+         
+ 
+
+      
+            start_layer_index = end_layer_index
+            confidence, location = self.compute_header(header_index, y)
+            header_index += 1
+            confidences.append(confidence)
+            locations.append(location)
+
+        for layer in self.base_net[end_layer_index:]:
+            x = layer(x)
+
+        for layer in self.extras:
+            x = layer(x)
+            confidence, location = self.compute_header(header_index, x)
+            header_index += 1
+            confidences.append(confidence)
+            locations.append(location)
+
+        confidences = torch.cat(confidences, 1)
+        locations = torch.cat(locations, 1)
+        
+        if self.is_test:
+            confidences = F.softmax(confidences, dim=2)
+            boxes = box_utils.convert_locations_to_boxes(
+                locations, self.priors, self.config.center_variance, self.config.size_variance
+            )
+            boxes = box_utils.center_form_to_corner_form(boxes)
+            return confidences, boxes
+        else:
+            return confidences, locations
+            
+
+    def compute_header(self, i, x):
+
+        confidence = self.classification_headers[i](x)
+        confidence = confidence.permute(0, 2, 3, 1).contiguous()
+        confidence = confidence.view(confidence.size(0), -1, self.num_classes)
+        location = self.regression_headers[i](x)
+        location = location.permute(0, 2, 3, 1).contiguous()
+        location = location.view(location.size(0), -1, 4)
+        return confidence, location
+
+    def init_from_base_net(self, model):
+        self.base_net.load_state_dict(torch.load(model, map_location=lambda storage, loc: storage), strict=True)
+        self.source_layer_add_ons.apply(_xavier_init_)
+        self.extras.apply(_xavier_init_)
+        self.classification_headers.apply(_xavier_init_)
+        self.regression_headers.apply(_xavier_init_)
+
+    def init_from_pretrained_ssd(self, model):
+        state_dict = torch.load(model, map_location=lambda storage, loc: storage)
+        state_dict = {k: v for k, v in state_dict.items() if not (k.startswith("classification_headers") or k.startswith("regression_headers"))}
+        model_dict = self.state_dict()
+        model_dict.update(state_dict)
+        self.load_state_dict(model_dict)
+        self.classification_headers.apply(_xavier_init_)
+        self.regression_headers.apply(_xavier_init_)
+
+    def init(self):
+        self.base_net.apply(_xavier_init_)
+        self.source_layer_add_ons.apply(_xavier_init_)
+        self.extras.apply(_xavier_init_)
+        self.classification_headers.apply(_xavier_init_)
+        self.regression_headers.apply(_xavier_init_)
+
+    def load(self, model):
+        self.load_state_dict(torch.load(model, map_location=lambda storage, loc: storage))
+
+    def save(self, model_path):
+        torch.save(self.state_dict(), model_path)
+
+
+class MatchPrior(object):
+    def __init__(self, center_form_priors, center_variance, size_variance, iou_threshold):
+        self.center_form_priors = center_form_priors
+        self.corner_form_priors = box_utils.center_form_to_corner_form(center_form_priors)
+        self.center_variance = center_variance
+        self.size_variance = size_variance
+        self.iou_threshold = iou_threshold
+
+    def __call__(self, gt_boxes, gt_labels):
+        if type(gt_boxes) is np.ndarray:
+            gt_boxes = torch.from_numpy(gt_boxes)
+        if type(gt_labels) is np.ndarray:
+            gt_labels = torch.from_numpy(gt_labels)
+        boxes, labels = box_utils.assign_priors(gt_boxes, gt_labels,
+                                                self.corner_form_priors, self.iou_threshold)
+        boxes = box_utils.corner_form_to_center_form(boxes)
+        locations = box_utils.convert_boxes_to_locations(boxes, self.center_form_priors, self.center_variance, self.size_variance)
+        return locations, labels
+
+
+def _xavier_init_(m: nn.Module):
+    if isinstance(m, nn.Conv2d):
+        nn.init.xavier_uniform_(m.weight)
