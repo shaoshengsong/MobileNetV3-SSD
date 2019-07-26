de7a8926a6592029ca914de489c14f746e1196cf
shaoshengsong <30892500+shaoshengsong@users.noreply.github.com>
2019-07-26 09:48:35 +0800

    Update convert_to_caffe2_models.py

--- convert_to_caffe2_models.py
+++ convert_to_caffe2_models.py
@@ -2,11 +2,11 @@ from vision.ssd.vgg_ssd import create_vgg_ssd
 from vision.ssd.mobilenetv1_ssd import create_mobilenetv1_ssd
 from vision.ssd.mobilenetv1_ssd_lite import create_mobilenetv1_ssd_lite
 from vision.ssd.squeezenet_ssd_lite import create_squeezenet_ssd_lite
-
+from vision.ssd.mobilenet_v3_ssd_lite import create_mobilenetv3_ssd_lite
 import sys
 import torch.onnx
-from caffe2.python.onnx.backend import Caffe2Backend as c2
-import onnx
+#from caffe2.python.onnx.backend import Caffe2Backend as c2
+#import onnx
 
 
 if len(sys.argv) < 3:
@@ -28,6 +28,8 @@ elif net_type == 'mb1-ssd-lite':
     net = create_mobilenetv1_ssd_lite(len(class_names), is_test=True)
 elif net_type == 'sq-ssd-lite':
     net = create_squeezenet_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'mb3-ssd-lite':
+    net = create_mobilenetv3_ssd_lite(len(class_names), is_test=True)    
 else:
     print("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
     sys.exit(1)
@@ -42,7 +44,7 @@ predict_net_txt_path = f"models/{net_type}_predict_net.pbtxt"
 
 dummy_input = torch.randn(1, 3, 300, 300)
 torch.onnx.export(net, dummy_input, model_path, verbose=False, output_names=['scores', 'boxes'])
-
+'''
 model = onnx.load(model_path)
 init_net, predict_net = c2.onnx_graph_to_caffe2_net(model)
 
@@ -59,3 +61,4 @@ with open(init_net_txt_path, 'w') as f:
 
 with open(predict_net_txt_path, 'w') as f:
     f.write(str(predict_net))
+'''    
